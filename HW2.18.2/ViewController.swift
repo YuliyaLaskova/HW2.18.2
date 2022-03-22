7af00b9871610c1cb9fb2187a645f728987b1df9
Yuliya <101642263+YuliyaLaskova@users.noreply.github.com>
2022-03-22 16:19:45 +0300

    Initial Commit

---
+++ HW2.18.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.18.2
+//
+//  Created by Yuliya Laskova on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
