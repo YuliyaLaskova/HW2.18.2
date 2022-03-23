600018f477dae354f5539235bdad2787a7b6de5e
Yuliya <101642263+YuliyaLaskova@users.noreply.github.com>
2022-03-23 16:19:19 +0300

    deleted duplicating of code, created new methods to do the same tasks. App is finished

--- HW2.18.2/ViewController.swift
+++ HW2.18.2/ViewController.swift
@@ -27,29 +27,66 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         
-        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+        /* 1.1 coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                               green: CGFloat(greenSlider.value),
                                               blue: CGFloat(blueSlider.value),
-                                              alpha: 1)
+                                              alpha: 1)*/
         
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-
+        setViewColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
     }
 
+    /*
+   2. redLabel.text = String(format: "%.2f", redSlider.value)
+    greenLabel.text = String(format: "%.2f", greenSlider.value)
+    blueLabel.text = String(format: "%.2f", blueSlider.value)
+*/
+    
     @IBAction func rgbSlider(_ sender: UISlider) {
+        setViewColor()
+        
+        switch sender.tag {
+        case 0: redLabel.text = string(from: sender)
+        case 1: greenLabel.text = string(from: sender)
+        case 2: blueLabel.text = string(from: sender)
+        default: break
+        }
+    }
+    /* 1. метод который мы использовали бы для изменения цвета ВЬЮ, но тогда будет дублирование кода, так как присвоение цвета выше выглядело также. Чтобы это избежать создаем отельный приватный метод SERCOLOR и передаем уже его
+     
+     1.1 coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                          green: CGFloat(greenSlider.value),
+                                          blue: CGFloat(blueSlider.value),
+                                          alpha: 1)
+
+   2. тоже самое делвем для лейблов.
+    
+    redLabel.text = String(format: "%.2f", redSlider.value)
+    greenLabel.text = String(format: "%.2f", greenSlider.value)
+    blueLabel.text = String(format: "%.2f", blueSlider.value)
+   */
+
+    
+    private func setViewColor() {
         coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                               green: CGFloat(greenSlider.value),
                                               blue: CGFloat(blueSlider.value),
                                               alpha: 1)
-        
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-        
     }
     
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redLabel.text = string(from: redSlider)
+            case 1: greenLabel.text = string(from: greenSlider)
+            case 2: blueLabel.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
     
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
